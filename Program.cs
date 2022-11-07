7028f20219263aeb9a3c8131b3187410c7c91e9c
giveo <giveo@CORP>
2022-11-07 16:49:45 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+namespace single_linked_list
+{
+    // each node consist of the information part and link to the next node
+    class Node
+    {
+        public int rollNumber;
+        public string name;
+        public Node next;
+    }
+
+    class List
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
